48216c3f6a872a86dbd402a3aa1f0b6194e98693
Diane Shan <dianeshan40785@gmail.com>
2022-05-09 21:58:27 -0700

    Change name of route

--- lab5/book-api.js
+++ lab5/book-api.js
@@ -8,30 +8,7 @@ const app = express();
 const port = 3000;
 
 // where we will keep the books
-let books = [
-    {
-            "isbn":"9781593279509",
-            "title":"Eloquent JavaScript, Third Edition",
-            "subtitle":"A Modern Introduction to Programming",
-            "author":"Marijn Haverbeke",
-            "published":"2018-12-04T00:00:00.000Z",
-            "publisher":"No Starch Press",
-            "pages":472,
-            "description":"JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.",
-            "website":"http://eloquentjavascript.net/"
-        },
-        {
-            "isbn":"9781491943533",
-            "title":"Practical Modern JavaScript",
-            "subtitle":"Dive into ES6 and the Future of JavaScript",
-            "author":"Nicolás Bevacqua",
-            "published":"2017-07-16T00:00:00.000Z",
-            "publisher":"O'Reilly Media",
-            "pages":334,
-            "description":"To get the most out of modern JavaScript, you need learn the latest features of its parent specification, ECMAScript 6 (ES6). This book provides a highly practical look at ES6, without getting lost in the specification or its implementation details.",
-            "website":"https://github.com/mjavascript/practical-modern-javascript"
-        },
-];
+let books = [];
 
 app.use(methodOverride('_method'))
 app.use(cors());
@@ -41,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/src', express.static(path.join(__dirname + '/src')));
 
-app.get('/book', (req, res) => {
+app.get('/book-list', (req, res) => {
     res.sendFile(__dirname + '/book-list.html');
 })
 
@@ -57,7 +34,7 @@ app.post('/book', (req, res) => {
     books.push(book);
 
     console.log('book is added');
-    res.redirect('/book');
+    res.redirect('/book-list');
     //res.send('Book is added to the database');
 });
 
@@ -94,7 +71,7 @@ app.post('/book/:isbn', (req, res) => {
     }
 
     console.log('book is editted');
-    res.redirect('/book');
+    res.redirect('/book-list');
     // res.send('Book is edited');
 })
 
@@ -106,7 +83,7 @@ app.delete('/book/:isbn', (req, res) => {
     books = books.filter(b => b.isbn !== isbn)
 
     console.log('book is deleted');
-    res.redirect('/book');
+    res.redirect('/book-list');
     //res.send('Book is deleted');
 });
 
